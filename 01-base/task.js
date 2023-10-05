dbec664c2f0a55532b53416a855049135ea2bfbb
Sergey Shimin <sergey.shimin84@gmail.com>
2023-10-05 16:44:58 +0700

    2. Основы JavaScript, ч.1

--- 01-base/task.js
+++ 01-base/task.js
@@ -21,15 +21,48 @@
 // console.log(a);
 
 //Задание №3
-let obj = {
-    productName: 'Яблоко',
-    productPrice: 10,
-    productQuantity: 3,
-    resultCost: +this.productPrice * +this.thiproductQuantity,
-}
+// const productName = 'Яблоко';
+// let productPrice = 10;
+// let productQuantity = 3;
+// let resultCost = +productPrice * +productQuantity;
 
-function result (obj) {
-    return console.log(`Вы купили ${obj.productName} (${obj.productQuantity} шт.) по ${obj.productPrice} ₽. Всего было потрачено: ${obj.resultCost} ₽`);
-}
+// console.log(`Вы купили ${productName} (${productQuantity} шт.) по ${productPrice} ₽. Всего было потрачено: ${resultCost} ₽`);
 
-result(obj);
+//Задание №4
+// typeof 'Hello'; //string 
+// typeof 10; //number
+// typeof {}; //object
+// typeof null; //object (ошибка языка JS)
+// typeof undefined; //undefined
+// typeof true; //boolean
+// typeof []; //object (массив - объект)
+// typeof (() => {}); //function
+
+//Задание №5
+// let arr = [
+//     10 == 10, //true
+//     10 != 10, //false
+//     12 == 'Привет', //false (при нестрогом сравнении JS преобразует строку в число)
+//     10 < 11, //true
+//     12 > 20, //false
+//     true && true, //true
+//     true && false, //false
+//     false && true, //false
+//     false || true, //true
+//     true || false, //true
+//     true || true, //true
+//     10 <= 10, //true
+//     18 <= 20, //true
+//     50 >= 50, //true
+//     51 >= 50, //true
+//     {} === {}, //false (фактически это два разных объекта)
+//     {} == {}, //false (фактически это два разных объекта)
+// ]
+
+// console.log(arr);
+
+//Задание №6
+// let a = 10;
+// let b = 5;
+
+// console.log((a > b) ? true : false);
