2ca306c170ea851703bcc4aaafa45f58d1640200
Sergey Shimin <sergey.shimin84@gmail.com>
2023-10-14 13:00:21 +0700

    4. Массивы и объекты

--- 02-array-object-start/app.js
+++ 02-array-object-start/app.js
@@ -14,4 +14,44 @@ console.log(array);
 array[array.length] = 'meat';
 */
 
+const inputElement = document.getElementById('title');
+const createBtn = document.getElementById('create');
+const listElement = document.getElementById('list');
+
+// console.log(inputElement.value);
+
+const notes = ['записать блок про массивы', 'рассказать теорию объектов'];
+
+function render() {
+    listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[0]));
+    listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[1]));
+}
+
+render();
+
+createBtn.onclick = function () {
+    if (inputElement.value.length === 0) {
+        return //undefined (сообщение не добавляется, завершается работа функции)
+    };
+    // listElement.innerHTML =
+    listElement.insertAdjacentHTML(
+        'beforeend', 
+        getNoteTemplate(inputElement.value)
+    );
+    inputElement.value = '';
+}
+
+function getNoteTemplate (title) {
+    return `
+        <li
+        class="list-group-item d-flex justify-content-between align-items-center"
+        > 
+        <span>${title}</span>
+        <span>
+            <span class="btn btn-small btn-success">&check;</span>
+            <span class="btn btn-small btn-danger">&times;</span>
+        </span>
+        </li>
+    `
+}
 
