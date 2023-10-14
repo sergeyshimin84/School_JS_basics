1d43b3369a026172b96dcf5e49cb8e06cded0f70
Sergey Shimin <sergey.shimin84@gmail.com>
2023-10-14 11:38:27 +0700

    4. Массивы и объекты

---
+++ 02-array-object-start/app.js
@@ -0,0 +1,17 @@
+/* Theory
+const array = [1, 2, 3, 4, 5, 33];
+const arrayStrings = ['a', 'b', 'c', null, 22];
+const arrayNew = new Array(1, 2, 3, 4, 5, 22, 44);
+console.log(array);
+console.log(arrayStrings);
+console.log(arrayNew);
+console.log(array.length);
+console.log(array[0]);
+console.log(array[array.length - 1]);
+console.log(array[10]);
+array[0] = 'Hi!';
+console.log(array);
+array[array.length] = 'meat';
+*/
+
+
