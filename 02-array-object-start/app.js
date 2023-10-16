e37e6ccb6117114861e6c1b9db8f4522b8b6c339
Sergey Shimin <sergey.shimin84@gmail.com>
2023-10-16 22:21:52 +0700

    4. Массивы и объекты

--- 02-array-object-start/app.js
+++ 02-array-object-start/app.js
@@ -1,4 +1,4 @@
-/* Theory
+/** Theory
 const array = [1, 2, 3, 4, 5, 33];
 const arrayStrings = ['a', 'b', 'c', null, 22];
 const arrayNew = new Array(1, 2, 3, 4, 5, 22, 44);
@@ -20,48 +20,47 @@ const listElement = document.getElementById('list');
 
 // console.log(inputElement.value);
 
-const notes = ['записать блок про массивы', 'рассказать теорию объектов'];
+// const notes = ['записать блок про массивы', 'рассказать теорию объектов'];
 
-function render() {
-    // for(let i = 0; i < notes.length; i++) {
-    //     listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i]));    
-    // }
+// function render() {
+//     // for(let i = 0; i < notes.length; i++) {
+//     //     listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i]));    
+//     // }
 
-    for(let note of notes) {
-        listElement.insertAdjacentHTML('beforeend', getNoteTemplate(note));    
-    }
-}
+//     for(let note of notes) {
+//         listElement.insertAdjacentHTML('beforeend', getNoteTemplate(note));    
+//     }
+// }
 
-render();
+// render();
 
-createBtn.onclick = function () {
-    if (inputElement.value.length === 0) {
-        return //undefined (сообщение не добавляется, завершается работа функции)
-    };
-    // listElement.innerHTML =
-    listElement.insertAdjacentHTML(
-        'beforeend', 
-        getNoteTemplate(inputElement.value)
-    );
-    inputElement.value = '';
-}
+// createBtn.onclick = function () {
+//     if (inputElement.value.length === 0) {
+//         return //undefined (сообщение не добавляется, завершается работа функции)
+//     };
+//     // listElement.innerHTML =
+//     listElement.insertAdjacentHTML(
+//         'beforeend', 
+//         getNoteTemplate(inputElement.value)
+//     );
+//     inputElement.value = '';
+// }
 
-function getNoteTemplate (title) {
-    return `
-        <li
-        class="list-group-item d-flex justify-content-between align-items-center"
-        > 
-        <span>${title}</span>
-        <span>
-            <span class="btn btn-small btn-success">&check;</span>
-            <span class="btn btn-small btn-danger">&times;</span>
-        </span>
-        </li>
-    `
-}
+// function getNoteTemplate (title) {
+//     return `
+//         <li
+//         class="list-group-item d-flex justify-content-between align-items-center"
+//         > 
+//         <span>${title}</span>
+//         <span>
+//             <span class="btn btn-small btn-success">&check;</span>
+//             <span class="btn btn-small btn-danger">&times;</span>
+//         </span>
+//         </li>
+//     `
+// }
 
-/**
- * Object Theory
+/** Object Theory
 
 const person = {
     firstName: 'Sergey',
@@ -76,4 +75,76 @@ person.getFullName();
 console.log(person['age']);
 // console.log(typeof person); //Object
 */
+
+const notes = [
+    {
+        title: 'записать блок про массивы',
+        completed: false,
+    },
+    {
+        title: 'рассказать теорию объектов',
+        completed: true, // Выполненно по умолчанию
+    }, 
+];
+
+function render() {
+    listElement.innerHTML = '';
+    
+    if (notes.length === 0) {
+        listElement.innerHTML = '<p>Нет элементов<p>';
+    }
+
+    for(let i = 0; i < notes.length; i++) {
+        listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i], i));    
+    }
+
+    // for(let note of notes) {
+    //     listElement.insertAdjacentHTML('beforeend', getNoteTemplate(note));    
+    // }
+}
+
+render();
+
+createBtn.onclick = function () {
+    if (inputElement.value.length === 0) {
+        return //undefined (сообщение не добавляется, завершается работа функции)
+    };
+    const newNotes = {
+        title: inputElement.value,
+        completed: false,
+    };
+    notes.push(newNotes);
+    render();
+    inputElement.value = ''
+}
+
+listElement.onclick = function (event) {
+    if (event.target.dataset.index) {
+        const index = Number(event.target.dataset.index);
+        const type = event.target.dataset.type;
+
+        if (type === 'toggle') {
+            notes[index].completed = !notes[index].completed
+        } else if (type === 'remove') {
+            notes.splice(index, 1);
+        }
+        
+        render();
+    }
+}
+
+function getNoteTemplate (note, index) {
+    return `
+        <li
+        class="list-group-item d-flex justify-content-between align-items-center"
+        > 
+        <span class="${note.completed ? 'text-decoration-line-through' : ''}">${note.title}</span>
+        <span>
+            <span class="btn btn-small btn-${note.completed ? 'warning' : 'success'}"
+            data-index="${index}" data-type="toggle">&check;</span>
+            <span class="btn btn-small btn-danger" data-index="${index}" data-type="remove">&times;</span>
+        </span>
+        </li>
+    `
+}
 
