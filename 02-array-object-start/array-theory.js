7d4395492aafa3f5b89b9072d31736bef604df3e
Sergey Shimin <sergey.shimin84@gmail.com>
2023-11-12 16:04:39 +0700

    4. Массивы и объекты

--- 02-array-object-start/array-theory.js
+++ 02-array-object-start/array-theory.js
@@ -1,4 +1,4 @@
-const names = ['Сергей', 'Наталья', 'Андрей', 'Ольга'];
+const names = ['Сергей', 'Наталья', 'Андрей', 'Ольга', 'Елена'];
 
 // names.push('Алена'); //быстрый метод
 // names.unshift('Таня') //медленный метод (перебирает весь массив, переназначает индексы)
@@ -19,3 +19,24 @@ const names = ['Сергей', 'Наталья', 'Андрей', 'Ольга'];
 // console.log(names.splice(0, 2)); //удаляет два элемента начиная с нулевого, меняет массив
 // console.log(names.toSpliced(2, 2)); //удаляет два элемента начиная с третьего, не менят исходный массив
 // console.log(names);
+
+const greateWoman = 'Елена';
+const index = names.indexOf(greateWoman);
+console.log(index);
+const newNames = names.with(index, 'Елена Великая')
+// console.log(index);
+// console.log(names[index]);
+// console.log(names);
+// console.log(newNames);
+
+// const capitalNames = names.map((name) => name.toUpperCase() + '!');
+const capitalNames = names.map(function
+    (name, index) {
+        if (index === 4) {
+           return name.toUpperCase() + '!'
+        }
+        return name;
+    });
+console.log(capitalNames);
+
+console.log(names.includes('Сергей'));
