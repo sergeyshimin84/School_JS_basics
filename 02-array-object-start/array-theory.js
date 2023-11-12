9d08c8d9737138d7c2c995d7320bf525f6c4c2f6
Sergey Shimin <sergey.shimin84@gmail.com>
2023-11-12 16:47:43 +0700

    4. Массивы и объекты

--- 02-array-object-start/array-theory.js
+++ 02-array-object-start/array-theory.js
@@ -1,4 +1,4 @@
-const names = ['Сергей', 'Наталья', 'Андрей', 'Ольга', 'Елена'];
+// const names = ['Сергей', 'Наталья', 'Андрей', 'Ольга', 'Елена'];
 
 // names.push('Алена'); //быстрый метод
 // names.unshift('Таня') //медленный метод (перебирает весь массив, переназначает индексы)
@@ -20,23 +20,43 @@ const names = ['Сергей', 'Наталья', 'Андрей', 'Ольга', '
 // console.log(names.toSpliced(2, 2)); //удаляет два элемента начиная с третьего, не менят исходный массив
 // console.log(names);
 
-const greateWoman = 'Елена';
-const index = names.indexOf(greateWoman);
-console.log(index);
-const newNames = names.with(index, 'Елена Великая')
+// const greateWoman = 'Елена';
+// const index = names.indexOf(greateWoman);
+// console.log(index);
+// const newNames = names.with(index, 'Елена Великая')
 // console.log(index);
 // console.log(names[index]);
 // console.log(names);
 // console.log(newNames);
 
 // const capitalNames = names.map((name) => name.toUpperCase() + '!');
-const capitalNames = names.map(function
-    (name, index) {
-        if (index === 4) {
-           return name.toUpperCase() + '!'
-        }
-        return name;
-    });
-console.log(capitalNames);
+// const capitalNames = names.map(function
+//     (name, index) {
+//         if (index === 4) {
+//            return name.toUpperCase() + '!'
+//         }
+//         return name;
+//     });
+// console.log(capitalNames);
 
-console.log(names.includes('Сергей'));
+// console.log(names.includes('Сергей'));
+
+const people = [
+    { name: 'Сергей', budget: 4200 }, 
+    { name: 'Наталья', budget: 15100 }, 
+    { name: 'Андрей', budget: 1200 }, 
+    { name: 'Ольга', budget: 7500 }, 
+    { name: 'Елена', budget: 8600 }, 
+];
+
+// const finedPerson = people.find(function
+//     (person) {
+//         return person.budget === 7500;
+//     });
+
+// console.log(finedPerson);
+
+// const filtered = people.filter(function (p) {
+//     return p.budget > 5000;
+// });
+// console.log(filtered);
