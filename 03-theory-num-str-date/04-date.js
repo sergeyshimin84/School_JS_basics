ed656b5d5f7a9232a2c3c431368226b5d28f2fd4
Sergey Shimin <sergey.shimin84@gmail.com>
2023-11-26 15:31:38 +0700

    5. Теоретическая база JS. Часть 1

---
+++ 03-theory-num-str-date/04-date.js
@@ -0,0 +1,21 @@
+// console.log(new Date()); // получаем тукущую дату и время
+// const now = new Date();
+
+// const start = new Date(0);
+// console.log(start); // дата отчета времени в JS
+
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getHours());
+// console.log(now.getMinutes());
+// console.log(now.getSeconds());
+// console.log(now.getMilliseconds());
+
+// now.setFullYear(2055); // передаем год
+// console.log(now);
+
+// console.log(now.toDateString()); // получаем строку с датой
+// console.log(now.toTimeString()); // получаем строку с временем
+// console.log(now.toLocaleDateString()); // получаем строку с датой в формате нашего региона
+// console.log(now.toLocaleTimeString()); // получаем строку с временем в формате нашего региона
