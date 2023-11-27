68cc45d6a959bc18824dcd9a1d00ee699ddb6e0a
Sergey Shimin <sergey.shimin84@gmail.com>
2023-11-27 17:12:23 +0700

    5. Теоретическая база JS. Часть 1

--- 03-theory-num-str-date/04-date.js
+++ 03-theory-num-str-date/04-date.js
@@ -19,3 +19,49 @@
 // console.log(now.toTimeString()); // получаем строку с временем
 // console.log(now.toLocaleDateString()); // получаем строку с датой в формате нашего региона
 // console.log(now.toLocaleTimeString()); // получаем строку с временем в формате нашего региона
+
+// Приложение
+
+let mode = 'time'
+const output = document.getElementById('output');
+const fullBtn = document.getElementById('full');
+const dateBtn = document.getElementById('date');
+const timeBtn = document.getElementById('time');
+
+fullBtn.onclick = function() {
+    mode = 'full'
+    update() // вызываем update чтобы избежать задержки при переключении, обновляет output
+};
+dateBtn.onclick = function() {
+    mode = 'date'
+    update()
+};
+timeBtn.onclick = function() {
+    mode = 'time'
+    update()
+};
+
+// setInterval(() => {
+//     update()
+// }, 1000);
+setInterval(update, 1000); // можем сократить запись, т.к. update не принимает никаких параметров
+update();
+
+function update() {
+    output.textContent = format(mode)
+};
+
+// Pure Function
+function format(formatMode) {
+    const now = new Date(); 
+    switch (formatMode) {
+        case 'time':
+            return now.toLocaleTimeString()
+        case 'date':
+            return now.toLocaleDateString()
+        case 'full':
+            return now.toLocaleDateString() + ' ' + now.toLocaleTimeString()
+        default:
+            return now.toLocaleTimeString()
+    }
+}
