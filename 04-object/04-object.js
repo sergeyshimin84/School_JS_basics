b12ccf559236a6e7793f17e7250a057d3d0ae773
Sergey Shimin <sergey.shimin84@gmail.com>
2023-11-29 18:04:41 +0700

    5. Теоретическая база JS. Часть 1

--- 04-object/04-object.js
+++ 04-object/04-object.js
@@ -12,6 +12,53 @@ const person = {
     greet() {
         console.log('Greet from person')
     },
+    arrow: () => {
+        console.log('Person Arrow') // this в стрелочной функции ссылается на глобальный объект window
+    },
+    info() {
+        // console.log('Person name:', person.name)
+        console.log('Person name:', this.name) // this указывает на объект в контексте которого находится
+    },
 }
 
-console.log(person);
+// console.log(person);
+// person.arrow();
+// person.info();
+// const addressKey = 'address';
+// console.log(person[addressKey]);
+// console.log(person['complex key']);
+
+// person.age++;
+// person.languages.push('de');
+
+// console.log(person.age);
+// console.log(person.languages);
+
+// delete person.address;
+// console.log(person);
+
+// const name = 'Petr';
+
+// const { age, name: firstName = 'TEST', languages } = person; // деструктуризация объекта, меняем значения и устанавливаем свойство по умолчанию
+
+// console.log(languages, age, firstName);
+
+// Итерация объектов
+
+// for (let key in person) {
+//     if (person.hasOwnProperty(key)) {
+//         console.log(person[key])
+//     } // корректое получение значений объекта
+//     // console.log(key) // выводим ключ
+//     // console.log(person[key]) // выводим свойства
+// };
+
+// Object.keys(person).forEach((key) => {
+//     console.log(person[key])
+// }); // keys возвращает массив, проходим по нему с помощью forEach, выводим в консоль
+
+const logger = {
+    keys() {
+        console.log('')
+    }
+}
