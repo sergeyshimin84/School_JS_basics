3bb76369af9d509014c7c97a4f3356d4ccf0c38f
Sergey Shimin <sergey.shimin84@gmail.com>
2023-12-24 13:55:25 +0700

    modification tasks/theory_part-4

--- tasks/theory_part-4.js
+++ tasks/theory_part-4.js
@@ -5,6 +5,18 @@
 //     let now = new Date();
 //     return now.getFullYear() - date.getFullYear();
 // }
+// Решение преподавателя
+// function calculateAge(birthDate) {
+//     const today = new Date();
+//     let age = today.getFullYear() - birthDate.getFullYear();
+//     const monthDiff = today.getMonth() - birthDate.getMonth();
+  
+//     if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+//       age--;
+//     } // Производим проверку на месяц и день рождения
+  
+//     return age;
+//   }
 
 // const birthDate = new Date('1990-05-20');
 // const age = calculateAge(birthDate);
@@ -25,6 +37,14 @@
 //     }
 //     return difference;
 // }
+// Решение преподавателя
+// function getDateDifference(date1, date2) {
+//     const oneDay = 24 * 60 * 60 * 1000;
+//     const diffInMilliseconds = Math.abs(date2 - date1);
+//     const diffInDays = Math.round(diffInMilliseconds / oneDay);
+  
+//     return diffInDays;
+// }
 
 // const date1 = new Date('2027-06-01');
 // const date2 = new Date('2027-06-10');
@@ -34,12 +54,28 @@
 // Задание #3
 // Напишите функцию getTimeUntilDate(date), 
 // которая возвращает объект с информацией о том, сколько времени осталось от текущей даты до переданной. 
+// Решение преподавателя
 // function getTimeUntilDate(date) {
-//     let now = new Date();
-//     let difference = date - now;
-//     let result = { days: d, hours: h, minutes: m, seconds: s } = difference;
-//     return result;
-// } // Не разобрался!
+//     const now = new Date();
+//     const timeDifference = date.getTime() - now.getTime();
+  
+//     if (timeDifference < 0) {
+//       return { days: 0, hours: 0, minutes: 0, seconds: 0 };
+//     }
+  
+//     const seconds = Math.floor(timeDifference / 1000);
+//     const minutes = Math.floor(seconds / 60);
+//     const hours = Math.floor(minutes / 60);
+//     const days = Math.floor(hours / 24);
+  
+//     return {
+//       days: days,
+//       hours: hours % 24,
+//       minutes: minutes % 60,
+//       seconds: seconds % 60
+//     };
+//   }
+
 // const targetDate = new Date('2027-12-31T23:59:59');
 // const timeUntilTargetDate = getTimeUntilDate(targetDate);
 // console.log(timeUntilTargetDate);
@@ -62,6 +98,9 @@
 //     let check = (days[n] == 'Воскресенье' || days[n] == 'Суббота') ? 'Выходной' : 'Будни';
 //     return `${check}: ${days[n]}`;
 // }
+// Решение преподавателя
+// const isWeekend = date => 
+// 	[6, 7].indexOf(date.getDay()) !== -1
 
 // const date1 = new Date('2027-07-10');  // Суббота
 // const date2 = new Date('2027-07-12');  // Понедельник
@@ -85,7 +124,7 @@
 // Задание #6
 // Напишите функцию, которая возвращает последнее число месяца.
 // function getLastDayOfMonth(year, month) {
-//     let date = new Date(year, month + 1, 0);
+//     const date = new Date(year, month + 1, 0);
 //     return date.getDate();
 // }
 
